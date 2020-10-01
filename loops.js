4081bf368de1891c1d2688c1caca12e92302c68d
looping-torrent-8105 <gianherrera96@gmail.com>
2020-10-01 03:06:58 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,23 @@
+function forLoop(array)
+{
+  for (let i = 0; i < 25; i++)
+  {
+    if(i === 1)
+     array[1] = "I am 1 strange loop.";
+
+    }else {
+     array[1]= `I am ${i} strange loops.`;
+    }
+    return array;
+  }
+}
+
+function whileLoop(n)
+{
+ while(n > 0)
+    {
+       console.log(n)
+        n = n - 1
+    }
+ return "Done"
+}
