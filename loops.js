9f286f73964042e2fb756fd1c905e312460eb6aa
looping-torrent-8105 <gianherrera96@gmail.com>
2020-10-01 03:48:42 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -26,5 +26,5 @@ function incrementVariable() {
 function doWhileLoop(number){
   do {
      console.log("I run once regardless.")
-  }while(incrementVariable() < number) ;
+  }while(incrementVariable() <= number) ;
 }
